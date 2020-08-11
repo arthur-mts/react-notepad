72235e828fc05587dc8998b4086639908428fc9a
Arthur Mauricio <arthur.mauricio@academico.ifpb.edu.br>
2020-08-11 10:33:35 -0300

    refactor: 💡 remove notes provider

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Routers from './src/routes';
-import { NotesProvider } from './src/hooks/Notes';
 
 import store from './src/store';
 
@@ -9,11 +8,8 @@ export default function App() {
   return (
     <>
       <Provider store={store}>
-        <NotesProvider>
-          <Routers />
-        </NotesProvider>
+        <Routers />
       </Provider>
-
     </>
   );
 }
